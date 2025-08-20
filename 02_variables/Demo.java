4ab4966b37b463f6d1f6fa36bd39a19341609f5a
Mangeshshinde8308 <mangeshshinde8308@gmail.com>
2025-08-20 15:39:32 +0530

    variable

---
+++ 02_variables/Demo.java
@@ -0,0 +1,25 @@
+package 02_variables;
+
+public class Demo {
+
+    //variables are container which poiniting memory address of stored value.
+
+    int a = 10; //here we are defining a as memory address of 10 where we stored this 10 value in memory. actual memory will like atuo3
+                //we can acess this memory using thsi variable a. so we can say variable is poninting to memory address of value 10.
+
+
+    int 2b;   //this is not valid variable name because variable name should not start with number. so this will give error.
+    int b2;   //this is valid variable name because variable name can start with letter or underscore(_).
+    System.out.println("Value of a is: " + a); //this will print value of a which is 10. means we are printing value which pointed by bariable a.
+
+
+    //Rule whiule define variable
+    // dont add any space in variable name.
+    // variable name should not start with number.
+    // variable name should not be reserved keyword.
+    // variable name should not contain special character except underscore(_).
+    // variable name should be meaningful.
+    // variable name should not be same as class name.
+    // variable name should not be same as method name.
+    //varaible name should not start with underscore(_).
+}
